7f76f3bb9b1b18297d7641c23865eddba3894556
Aftab <a_aftab.s@hotmail.com>
2022-10-26 22:33:56 +0500

    day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'dart:ffi';
+// import 'dart:ffi';
+// import 'dart:html';
 
-import 'package:catalog/home_page.dart';
+import 'package:catalog/pages/home_page.dart';
+import 'package:catalog/pages/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +14,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    // bringVegetables();
+    // bringVegetablesByOptionalParam(bag: true, money: 500);
+    // bringVegetablesByRequiredParam(bag: true);
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme:
+          ThemeData(brightness: Brightness.light, primarySwatch: Colors.amber),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
+
+  // Context =  Ye batata he k konsi chez ki kahan pr location he
+
+  // Note: If function created inside class that is call method.but if its created outside class that is called function
+  // by the way it is just a naming difference
+
+  bringVegetables() {
+    // Take Cycle
+    // Got to market
+    //purchase vegetables and pay for that
+    // get back to home
+  }
+
+  bringVegetablesByOptionalParam({bool bag = false, int money = 100}) {
+    // by default money = 100 and bag = true
+    // but if params passed then it will take those valies
+  }
+
+  bringVegetablesByRequiredParam(
+      {@required bool bag = false, int money = 100}) {
+    // by default money = 100 and bag = true
+    // but if params passed then it will take those valies
   }
 }
